ff3854b2b2d45c7bf4f2277d1d52ef3e45120263
EmmaLu-ux <genhualu22@gmail.com>
2022-11-22 22:15:10 +0800

    react后台项目v1

---
+++ build/precache-manifest.cec52c7a7e8471c381fd9efb795e58e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93d8afdb8e67c7a5ba0cf7579a65eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f44d19a7b93b7b41634",
+    "url": "/static/css/2.68a22620.chunk.css"
+  },
+  {
+    "revision": "a071458085f2cecef6e8",
+    "url": "/static/css/main.aeb9c464.chunk.css"
+  },
+  {
+    "revision": "9f44d19a7b93b7b41634",
+    "url": "/static/js/2.a895ae2b.chunk.js"
+  },
+  {
+    "revision": "34056af8cf170c8fbc997ee6a7983b71",
+    "url": "/static/js/2.a895ae2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a071458085f2cecef6e8",
+    "url": "/static/js/main.e010c7a0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e010c7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d70ddc83a2d143ce1f3",
+    "url": "/static/js/runtime-main.65d1900d.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "7e4f007f483aa1ea3baedc913e82acc5",
+    "url": "/static/media/login-pancake.7e4f007f.webp"
+  },
+  {
+    "revision": "632662aa8a13ad8f867c9d53acc5eb6f",
+    "url": "/static/media/logo.632662aa.jpeg"
+  }
+]);
